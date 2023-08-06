b034521fdd8fc05be6a57599ecc8330e80b6febf
Aleksandre Nutsubidze <aleksandrenutsubidze@gmail.com>
2023-08-06 17:21:43 +0400

    optimize: dataTable.js

--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,15 +1,11 @@
 import "./HomePage.scss";
-// import Header from "../component/Header";
-// import Footer from "../component/Footer";
 import HomePageSection from "../component/HomePageSection";
 import Layout from "../Layout/Layout";
 
 const HomePage = () => {
   return (
     <Layout>
-      {/* <Header /> */}
       <HomePageSection />
-      {/* <Footer /> */}
     </Layout>
   );
 };
